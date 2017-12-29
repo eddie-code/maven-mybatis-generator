6a7bb96a68470dc0807d2036424858ea5b5d03d1
eddie-code <231071372@qq.com>
2017-12-29 10:54:15 +0800

    test

---
+++ src/main/java/com/lwc/test/Test.java
@@ -0,0 +1,15 @@
+package com.lwc.test;
+
+/**
+ * @author LiWeiCheng
+ * @description
+ * @date created in 2017/12/29  10:50
+ * @modified by
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
